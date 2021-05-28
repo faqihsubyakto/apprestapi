a20ed52a8953296174003d617b3c68a548d915bd
faqih <faqihsubyakto140599@gmail.com>
2021-05-28 08:05:19 +0700

    add file terbaru

--- controller.js
+++ controller.js
@@ -32,3 +32,41 @@ exports.tampilberdasarkanid = function (req, res) {
     }
   );
 };
+
+// menambahkan data
+exports.tambahMahasiswa = function (req, res) {
+  var nim = req.body.nim;
+  var nama = req.body.nama;
+  var jurusan = req.body.jurusan;
+
+  connection.query(
+    "INSERT INTO mahasiswa (nim,nama,jurusan) VALUES (?,?,?)",
+    [nim, nama, jurusan],
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok("Berhasil menambahkan data mahasiswa", res);
+      }
+    }
+  );
+};
+
+exports.ubahdatamahasiswa = function (req, res) {
+  var id = req.params.id;
+  var nim = req.body.nim;
+  var nama = req.body.nama;
+  var jurusan = req.body.jurusan;
+
+  connection.query(
+    "UPDATE mahasiswa SET nim =?, nama =?, jurusan =?  WHERE id_mahasiswa=?",
+    [nim, nama, jurusan, id],
+    function (error, rows, fileds) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok("berhasil mengubah data", res);
+      }
+    }
+  );
+};
