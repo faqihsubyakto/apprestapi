745b8528d2d72e81573b7804c7e68ed9033abfcb
faqih <faqihsubyakto140599@gmail.com>
2021-06-01 21:31:20 +0700

    nambah bro

--- midllewere/index.js
+++ midllewere/index.js
@@ -8,6 +8,6 @@ router.post("/api/v1/register", auth.registrasi);
 router.post("/api/v1/login", auth.login);
 
 // alamat yang perlu otorisasi
-router.get("/api/v1/rahasia", Verifikasi(2), auth.halamanrahasia);
+router.get("/api/v1/rahasia", Verifikasi(1), auth.halamanrahasia);
 
 module.exports = router;
