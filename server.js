a52d8f5013a98fd1484cc53b0bc9ddb535188100
faqih <faqihsubyakto140599@gmail.com>
2021-06-02 10:15:07 +0700

    file new2

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ routes(app);
 // daftarkan menu routes dari index
 app.use("/auth", require("./midllewere"));
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log(`Server started on port 3000`);
 });
